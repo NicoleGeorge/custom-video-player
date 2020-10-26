e735a8f63463c281cae2a2f2b067235cb9d89005
AdorkablyNicole <hello@nicolegeorge.me>
2020-10-26 17:24:27 +1030

    play/pause toggle function added and working

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ const timestamp = document.getElementById('timestamp');
 
 // Play & Pause video Functions
 function toggleVideoStatus() {
-  return true;
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
 }
 
 // update play/pause icon
 function updatePlayIcon() {
-  return true;
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
 }
 
 // update progress & timestamp
